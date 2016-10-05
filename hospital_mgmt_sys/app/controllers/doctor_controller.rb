1df5e34500e372a4932e06986b67a6b0de1e946c
Gautam <khushbu.goyal@berylsystems.com>
2016-10-05 19:46:42 +0530

    Added crud operation for in Hospital_mgmt_sys

--- hospital_mgmt_sys/app/controllers/doctor_controller.rb
+++ hospital_mgmt_sys/app/controllers/doctor_controller.rb
@@ -12,4 +12,28 @@ class DoctorController < ApplicationController
 		end
 		redirect_to new_doctor_path
 	end
+
+	def destroy
+
+		@doctor = Doctor.find(params[:id])
+  	if @doctor
+  	  @doctor.destroy
+  	  redirect_to index_doctor_path
+	end
+end
+
+	def edit
+		@doctor = Doctor.find(params[:id])
+	end
+
+	def update
+		@doctor = Doctor.find(params[:id])
+  	if @doctor.update(params[:doctor].permit(:name, :email, :phone_no, :experience, :education, :speciality))
+  	  redirect_to index_doctor_path
+	end
+end
+
+	def index
+		@doctors = Doctor.all
+	end
 end
--- hospital_mgmt_sys/config/routes.rb
+++ hospital_mgmt_sys/config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root to: "patient#index"
   get 'new_appointment', to: "appointment#new"
   post 'new_appointment', to: "appointment#create"
+  resources :doctor
   get 'new_doctor', to: "doctor#new"
   post 'new_doctor', to: "doctor#create"
+  get 'edit_doctor', to: "doctor#edit"
+  patch 'doctor/edit', to: "doctor#update"
+  get 'index_doctor', to: "doctor#index"
+  delete 'destroy_doctor', to: "doctor#destroy"
 end
